74478c91fb9642fa8a742f06177d656996192938
JamesKaluna <kalunajames@gmail.com>
2025-02-17 20:47:56 -0500

    Update findPetri.m

--- Lab 3/Lab3_code/findPetri.m
+++ Lab 3/Lab3_code/findPetri.m
@@ -1,16 +1,17 @@
 function [petri_center,petri_radius] = findPetri(current_frame)
-% TODO: copy and paste your findPetri from previous lab
-%Utilizing find petri from lab 2
 % Function used to locate the petri dish
-image = current_frame;
-dia = 932.5495;
-dia1 = round((dia -50)/2 - 50);
-dia2 = round((dia+50)/2)+50;
-image_white = imbinarize(image,'adaptive');
-[centers,radii] = imfindcircles(image_white,[dia1 dia2],Sensitivity=0.92,Method="TwoStage");
-k_max =  find(radii == max(radii));
-petri_center = centers(k_max,:);
-petri_radius = radii(k_max);
+
+    image = current_frame; 
+    % TODO: use im2bw to threshold the image and convert it to binary image
+    % you wll use the circle with maximum radius as the petri dish
+    
+    image_white = im2bw(image,0.45);
+    [centers,radii] = imfindcircles(image_white,[300 500],'ObjectPolarity','bright', ...
+    'Sensitivity',0.99);
+    k_max =  (radii == max(radii));
+    petri_center = centers(k_max,:);
+    petri_radius = radii(k_max);
+
 
 end
 
