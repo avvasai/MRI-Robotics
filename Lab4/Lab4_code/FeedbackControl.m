defa0a0bb1e976b04b3e28e968534ee5fed0da85
JamesKaluna <kalunajames@gmail.com>
2025-02-27 13:27:46 -0500

    Update FeedbackControl.m

--- Lab4/Lab4_code/FeedbackControl.m
+++ Lab4/Lab4_code/FeedbackControl.m
@@ -35,30 +35,16 @@ PID_x = [settings.p_control settings.i_control settings.d_control]*...
 PID_y = [settings.p_control settings.i_control settings.d_control]*...
     [kp*data.err_yPos; ki*data.sum_err_y; kd*err_y_dot];
 
-%Activation of Coils based on condition
-r_origin_x = data.curr_x
-r_origin_y = data.curr_y
-d_conv_x = data.desired_x - r_origin_x
-d_conv_y = data.desired_y - r_origin_y
-d_quad = quad(d_conv_x,d_conv_y)
-r_quad = quad(data.curr_x,data.curr_y)
-
-if d_quad == 2 || d_quad == 3
-    west = PID_x;
-elseif d_quad == 1 || d_quad == 4
+if data.err_xPos > 0 %if we have positive error, pull towards east coil
     east = PID_x;
-else
-    east = 0;
-    west = 0;
+else % if the error is negative, pull towards west coil 
+    west = PID_x;
 end
 
-if d_quad == 2 || d_quad == 1
+if data.err_yPos > 0 %if we have positive error, pull towards south coil
+    south =  PID_y;
+else % if the error is negative, pull towards north coil 
     north = PID_y;
-elseif d_quad == 4 || d_quad == 3
-    south = PID_y;
-else
-    south = 0;
-    north = 0;
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
